f4a166d6b1ed0a4c789b7940f56ded6697ef209b
egor-rusnak <yehor.rusnak9982@gmail.com>
2021-09-07 00:51:44 +0300

    Added api key check

--- Watchdog/Watchdog.Common/WatchdogSettingsBase.cs
+++ Watchdog/Watchdog.Common/WatchdogSettingsBase.cs
@@ -3,13 +3,16 @@
     public abstract class WatchdogSettingsBase
     {
         private const string _defaultApiEndPoint = "https://bsa-watchdog.westeurope.cloudapp.azure.com/collector/issues/client";
-        
+        private const string _defaultCoreEndPoint = "https://bsa-watchdog.westeurope.cloudapp.azure.com/api";
+
         protected WatchdogSettingsBase()
         {
             ApiEndpoint = _defaultApiEndPoint;
+            CoreEndpoint = _defaultCoreEndPoint;
         }
 
         public string ApiEndpoint { get; set; }
+        public string CoreEndpoint { get; set; }
 
         public string ApiKey { get; set; }
     }
